7d590728ed87df83ccc9af9aefc3045873bb092b
fancity-xia <659477532@qq.com>
2025-11-11 14:05:09 +0800

    add avatar

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-/ehpc/Software/hugo/hugo server --theme stack --configDir hugo.yaml --ignoreCache -p 1313 --bind 0.0.0.0
+/ehpc/Software/hugo/hugo server --theme stack --configDir hugo.yaml  -p 1313 --bind 0.0.0.0
